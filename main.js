ba0e08d989dd9de36c8ef3d63dd0a50606b7f878
scoderr <BahriddinBoboyev997@gmail.com>
2022-11-18 09:07:53 +0500

    function calculator complete!

---
+++ main.js
@@ -0,0 +1,77 @@
+
+let son1 = prompt("1-soni kiriting")*1;
+let amal = prompt("amalni kiriting +,-,*,/, lardan foydalaning")
+let son2 = prompt("2-soni kiriting")*1;
+
+
+// declaration function calculator
+
+function calculator (a, b , c){
+if(b == "+"){
+  return a + c
+}
+else if(b == "-"){
+  return a - c
+}
+else if(b == "*"){
+  return a * c
+}
+else if(b == "/"){
+  return a / c
+}
+else{
+  return "Siz xato buyruq yubordingiz! Bunday buyruq topilmadi! 😑"
+}
+}
+
+calculator(son1 , amal , son2);
+document.write(calculator(son1, amal, son2));
+
+// ////////////////////////////////////////////////////////////////
+
+// expression function calculator
+
+// const calc = function(a, b , c){
+// if(b == "+"){
+//   return a + c
+// }
+// else if(b == "-"){
+//   return a - c
+// }
+// else if(b == "*"){
+//   return a * c
+// }
+// else if(b == "/"){
+//   return a / c
+// }
+// else{
+//   return "Siz xato buyruq yubordingiz! Bunday buyruq topilmadi! 😑"
+// }
+// }
+
+// calc(son1 , amal , son2);
+// console.log(calc(son1, amal, son2));
+
+// ////////////////////////////////////////////////////////////////
+
+// switch function calculator
+
+// switch(amal){
+//   case "+":
+//    console.log(son1 + son2);
+//    break;
+//   case "-":
+//    console.log(son1 - son2);
+//    break;
+//   case "*":
+//    console.log(son1 * son2);
+//    break
+//   case "/":
+//    console.log(son1 / son2);
+//    break;
+//   default:
+//    console.log("Siz xato buyruq yubordingiz! Bunday buyruq topilmadi! 😑")
+//    break;
+// }
+
+// ////////////////////////////////////////////////////////////////
